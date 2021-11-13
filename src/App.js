bac9053c6c0284d24ccb0fb9b8a07d9154e4a06f
unknown <Elhosary@Elhosary.issi.isi>
2021-11-13 22:26:36 +0200

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import NewPlace from './places/pages/place'
 
+import Users from './user/pages/users'
 
-function App() {
-  return <h1>khello !</h1>
+const App =()=> {
+  return(
+ 
+    <Routes>
+   
+     <Route path="/"exact element={<Users/>}> </Route>
+     <Route path="/pages" exact element={<NewPlace/>}> </Route>
+   
+    </Routes>
+ 
+  )
 }
 
 export default App;
---
+++ src/places/pages/place.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const NewPlace=()=>{
+    return <h2>new place work</h2>
+};
+export default NewPlace;
