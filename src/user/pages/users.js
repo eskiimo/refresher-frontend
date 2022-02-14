8e20326227c27e1408b6dac7103150f385a33325
eskiimo <kareeemgm@gmail.com>
2022-02-14 23:41:28 +0200

    getting users

--- src/user/pages/users.js
+++ src/user/pages/users.js
@@ -1,25 +1,46 @@
-import React from "react";
-import UserList from "../components/UsersList";
+import React, { useEffect, useState } from 'react';
+import ErrorModal from '../../shared/components/UiElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UiElements/LoadingSpinner';
+import UserList from '../components/UsersList';
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "SAD Eskimo",
-      image:
-        "https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/72204059_2587847334625274_8755928279057170432_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFtA7mpjrDHNQ0SLkNMtWG5kdY_awScuT-R1j9rBJy5P8QflkvJW8GScJYp91UcZkMB2gbLKdOyMtQBHkzjvyuh&_nc_ohc=gDbV39t8P30AX_aXUh5&_nc_ht=scontent.fcai1-2.fna&oh=f96b7faa94e98806924f623570caf8f5&oe=61CAD2FB",
-      places: 3,
-    },
-    {
-      id: "u2",
-      name: "SAD Qadry",
-      image:
-        "https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/117841366_2825476494346875_7632852498606290540_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=174925&_nc_eui2=AeGraFTTmg5KEPNSF5Kwu2bOg3p5LiV-8uKDenkuJX7y4kUiwCuXov6AwuQG1zpinIDkPmkzdgIomNwUWdGKAOQr&_nc_ohc=hjpJxgjdn8sAX_e7bel&tn=-byC4irF1rsJOh_Q&_nc_ht=scontent.fcai1-2.fna&oh=9cdc319343cf12566e22347ae7c1d3d1&oe=61CC87DC",
-      places: 1,
-    },
-  ];
+   const [isloading, setIsLoading] = useState(false);
+   const [error, setError] = useState(false);
+   const [loadedUsers, setLoadedUsers] = useState([]);
+   useEffect(() => {
+      const getAllUsers = async () => {
+         setIsLoading(true);
+         try {
+            const response = await fetch('http://localhost:5000/api/users');
+            const responseData = await response.json();
+            if (!response.ok) {
+               throw new Error(responseData.message);
+            }
 
-  return <UserList items={USERS} />;
+            setLoadedUsers(responseData.users);
+         } catch (e) {
+            setError(e.message);
+         }
+
+         setIsLoading(false);
+      };
+      getAllUsers();
+   }, []);
+   const errorHandler = () => {
+      setError(null);
+   };
+
+   return (
+      <React.Fragment>
+         <ErrorModal error={error} onClear={errorHandler} />
+         {isloading && (
+            <div className="center">
+               <LoadingSpinner />
+            </div>
+         )}
+         {!isloading && loadedUsers && <UserList items={loadedUsers} />}
+      </React.Fragment>
+   );
 };
 
 export default Users;
