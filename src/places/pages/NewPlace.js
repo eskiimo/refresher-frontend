1906e20fa53e4d789fb4561bc32d052805f73c98
eskiimo <kareeemgm@gmail.com>
2022-02-26 13:01:45 +0200

    creator removed from req body

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -36,7 +36,6 @@ const NewPlace = () => {
          formData.append('title', formState.inputs.title.value);
          formData.append('desc', formState.inputs.desc.value);
          formData.append('address', formState.inputs.address.value);
-         formData.append('creator', auth.userId);
          formData.append('image', formState.inputs.image.value);
          await sendRequest(
             'http://localhost:5000/api/places',
